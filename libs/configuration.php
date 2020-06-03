36806d3c9e5f32588fb7c30cc9c1e20b3ba84ba0
Steven Vargas <stevendlavc@gmail.com>
2020-06-03 12:07:01 -0600

    Estructura base

---
+++ libs/configuration.php
@@ -0,0 +1,18 @@
+<?php
+
+//configuration
+require 'libs/Config.php';
+$config = Config::singleton();
+$config->set('controllerFolder', 'controller/');
+$config->set('modelFolder', 'model/');
+$config->set('viewFolder', 'view/');
+
+//$config->set('dbhost', 'localhost');
+//$config->set('dbname', 'gestion_estudiantil');
+//$config->set('dbuser', 'root');
+//$config->set('dbpass', '');
+$config->set('dbhost', '163.178.107.130');
+$config->set('dbname', 'gestion_estudiantil');
+$config->set('dbuser', 'laboratorios');
+$config->set('dbpass', 'UCRSA.118');
+?>
